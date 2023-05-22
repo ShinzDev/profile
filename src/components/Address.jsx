2fb111c219e02cecdca7477b78a9fa478079d908
Shinaayomi Bamgboye~ <shinaayomibamgboye@gmail.com>
2023-05-22 19:02:22 +0100

    profile

---
+++ src/components/Address.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Address = () => {
+  return (
+    <div>
+      <h1>17 Andrew street</h1>
+    </div>
+  )
+}
+
+export default Address
---
+++ src/components/Photo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './photo.css'
+const Photo = () => {
+  return (
+    <div>
+      <img src="ican.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Photo
